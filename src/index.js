f4d523c78fe3b7d16fa28cb4a95e3c7779ac9917
Abdirashidnoor <abdirashid16112@gmail.com>
2023-03-24 12:28:14 +0300

    assingment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // Get references to the input field, submit button, and task list
+  let taskInput = document.getElementById("new-task-description");
+  let submitBtn = document.getElementById("create-task-form").querySelector("input[type='submit']");
+  let taskList = document.getElementById("tasks");
+
+  // Add event listener to the submit button
+  submitBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Get the task string from the input field
+    let task = taskInput.value;
+
+    // Create a new list item element
+    let taskItem = document.createElement("li");
+
+    // Set the text content of the list item to the task string
+    taskItem.textContent = task;
+
+    // Append the list item to the task list
+    taskList.appendChild(taskItem);
+
+    // Reset the input field to empty
+    taskInput.value = "";
+  });
 });
+
